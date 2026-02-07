777857ea9128d5a5351e6db191f277bce16bbb9f
Surakit2538 <surakit.pattanatiensakul@gmail.com>
2026-02-07 16:21:39 +0700

    Update send-line.js

--- api/send-line.js
+++ api/send-line.js
@@ -138,7 +138,7 @@ export default async function handler(req, res) {
                             action: {
                                 type: "uri",
                                 label: "เปิดแอป Dept Money",
-                                uri: "https://dept-three.vercel.app/" // เปลี่ยนเป็น URL เว็บของคุณ
+                                uri: "https://dept-game.vercel.app/" // เปลี่ยนเป็น URL เว็บของคุณ
                             },
                             style: "primary",
                             color: "#15803d",
@@ -185,3 +185,4 @@ export default async function handler(req, res) {
         return res.status(500).json({ message: 'Internal Server Error', error: error.message });
     }
 }
+
